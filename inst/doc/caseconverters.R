77621b1e87159a6ef2b6bcd4e9804535e23263c6
Malte Grosser <malte.grosser@gmail.com>
2023-08-27 22:50:09 +0000

    version 0.11.1

--- inst/doc/caseconverters.R
+++ inst/doc/caseconverters.R
@@ -1,17 +1,17 @@
-## ---- collapse = TRUE----------------------------------------------------
+## ---- collapse = TRUE---------------------------------------------------------
 library(snakecase)
 to_any_case("veryComplicatedString")
 
-## ---- collapse = TRUE----------------------------------------------------
+## ---- collapse = TRUE---------------------------------------------------------
 to_any_case(names(iris), sep_in = "\\.", case = "upper_camel", sep_out = " ")
 
-## ---- collapse = TRUE----------------------------------------------------
+## ---- collapse = TRUE---------------------------------------------------------
 to_any_case("Doppelgänger is originally german", 
             transliterations = "german", case = "upper_camel")
 
-## ---- collapse = TRUE----------------------------------------------------
+## ---- collapse = TRUE---------------------------------------------------------
 to_any_case("THISIsHOW IAmPARSED!", case = "parsed")
 
-## ---- collapse = TRUE----------------------------------------------------
+## ---- collapse = TRUE---------------------------------------------------------
 dput(to_any_case(c("SomeBAdInput", "someGoodInput")))
 
